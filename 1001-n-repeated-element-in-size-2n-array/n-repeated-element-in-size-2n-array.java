385ba6bfa3c60347541b6b262bac331ae76f9885
Pallelli Lakshmi Tulasi <23BQ1A1286@vvit.net>
2026-01-02 17:28:20 +0530

    Time: 1 ms (65.88%) | Memory: 47.5 MB (36.21%) - LeetSync

---
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+        for(int num : nums){
+            if(seen.contains(num)){
+            return num;
+
+        }
+        seen.add(num);
+        }
+        return -1;
+    }
+    
+}
