b30cfba40800a5c62a5ec0aeae53983b8a7df993
Pallelli Lakshmi Tulasi <23BQ1A1286@vvit.net>
2026-01-26 13:15:46 +0530

    Time: 21 ms (72.27%) | Memory: 64.2 MB (20.20%) - LeetSync

---
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        Arrays.sort(arr);
+        List<List<Integer>> result = new ArrayList<>();
+        int minDiff = Integer.MAX_VALUE;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int diff = arr[i + 1] - arr[i];
+            if (diff < minDiff) {
+                minDiff = diff;
+                result.clear();
+                result.add(Arrays.asList(arr[i], arr[i + 1]));
+            } else if (diff == minDiff) {
+                result.add(Arrays.asList(arr[i], arr[i + 1]));
+            }
+        }
+        return result;
+    }
+}
